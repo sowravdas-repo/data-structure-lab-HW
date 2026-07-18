0c9806ca5cc7d16c698461b0676a6b364204d9ec
Sowrav Das <dassowrav466@gmail.com>
2026-07-18 19:44:09 +0600

    Deletion on LList

---
+++ Linked List/Deletation.c
@@ -0,0 +1,108 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct node{
+    int value;
+    struct node *nextptr;
+}node;
+
+node *createnode(int x){
+    node *newnode=(node*)malloc(sizeof(node));
+
+    if(newnode==NULL){
+        printf("memmory alloc failed!!\n");
+        //this failed solely bcz of system,when it run out of memory
+        return NULL;
+    }
+    newnode->value=x;
+    newnode->nextptr=NULL;
+    return newnode;
+}
+void insertfront(node **head,int value){
+    node *newnode=createnode(value);
+    newnode->nextptr=*head;
+    *head=newnode;
+}
+void insertback(node **head,int value){
+    node *newnode=createnode(value);
+    if(*head==NULL){
+        *head=newnode;
+        return;
+    }
+
+    node *tmp=*head;
+    while((*tmp).nextptr!=NULL){
+        tmp=(*tmp).nextptr;
+        //tmp=tmp->nextptr;
+    }
+    tmp->nextptr=newnode;
+}
+void insertposition(node **head,int value,int pos){
+    if(pos==1){
+        insertfront(head,value);
+        return;
+    }
+    else {
+        node *newnode=createnode(value);
+        node *tmp=*head;
+        for(int i=1;i<pos-1 && tmp!=NULL;i++){
+            tmp=tmp->nextptr;
+        }
+
+        if(tmp==NULL){
+            printf("Invalid positon !!\n");
+            free(newnode);
+            return;
+        }
+        newnode->nextptr=tmp->nextptr;
+        tmp->nextptr=newnode;
+
+    }
+
+}
+void deletefront(node **head){
+    if(*head==NULL){
+        printf("List is empty\n");
+        return;
+    }
+    node *tmp=*head;
+    *head=(*head)->nextptr;
+    free(tmp);
+}
+
+void deleteback(node **head){
+    if(*head==NULL){
+        printf("List is empty\n");
+        return;
+    }
+    if((*head)->nextptr==NULL){
+        free(*head);
+        *head=NULL;
+        return;
+    }
+    node *tmp=*head;
+    while(tmp->nextptr->nextptr!=NULL) tmp=tmp->nextptr;
+
+    free(tmp->nextptr);
+    tmp->nextptr=NULL;
+}
+void display(node *head){
+    node *tmp=head;
+    while(tmp!=NULL){
+        printf("%d ",(*tmp).value);
+        tmp=tmp->nextptr;
+    }
+    printf("\n");
+}
+int main(){
+  node *head=NULL;
+  insertfront(&head,10);
+  insertfront(&head,20);
+  insertback(&head,5);
+  insertposition(&head,99,2);
+  display(head);
+  deletefront(&head);
+   display(head);
+   deleteback(&head);
+   display(head);
+    return 0;
+}
