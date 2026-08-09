8785cf0b8fc37c7531b800820ab655507858b50b
Sowrav Das <dassowrav466@gmail.com>
2026-08-10 00:27:05 +0600

    Counting Sort Implementation

---
+++ Sorting Algorithms/CountingSort.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+void setAllzero(int freq[],int a,int b){
+   for(int i=a;i<b;i++) freq[i]=0;
+}
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    int k=-1; //initial valur for max element
+    for(int i=0;i<n;i++){
+         scanf("%d",&a[i]);
+         if(k < a[i]) k=a[i];
+    }
+
+    int freq[k+1];
+     setAllzero(freq,0,k+1);
+
+    //  for(int i=0;i<k+1;i++) printf("%d ",freq[i]);
+    for(int i=0;i<n;i++){
+        freq[a[i]]++;
+    }
+
+    //for(int i=0;i<k+1;i++) printf("%d ",freq[i]);
+    for(int i=1;i<=k;i++){
+        while(freq[i] > 0){
+            // printf("%d ",freq[a[i]]);
+            printf("%d ",i);
+            freq[i]--;
+        }
+    }
+    printf("\n");
+    return 0;
+}
